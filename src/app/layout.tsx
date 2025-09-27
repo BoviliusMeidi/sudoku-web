bfa3354eef7b65216ffbd9a132a52f6a8a216b61
BoviliusMeidi <boviliusbm@gmail.com>
2025-09-27 21:56:46 +0700

    change : metadata title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "SUDOKU Game",
+  title: "Sudoku.Bovi",
   description: "Sudoku Website for Portfolio - Next.js + Tailwind",
 };
 
